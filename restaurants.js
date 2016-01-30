8a77c1483a38f15f9aea8a5895e52e6d9ec63112
Joe Zhao <ztuowen@gmail.com>
2016-01-29 19:02:00 -0700

    commit from git, default

---
+++ restaurants.js
@@ -0,0 +1,3 @@
+data=[{"name":"Test1","open":[0,1,2,3,4,5,6],"detail":"default"},
+{"name":"Test2","open":[0,1,2,3,4,5,6],"detail":"default"},
+{"name":"Test3","open":[0,1,2,3,4,5,6],"detail":"default"}];
